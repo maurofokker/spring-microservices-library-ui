a406ecc9034127463cdbe984da3286a13475efe0
Mauricio <mgaldamesc.azurian@bancochile.cl>
2018-08-02 14:55:43 -0400

    set up simple interface to consume library catalog and reservation services

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring microservice library ui
+
+* This service is part of the simple library microservices project and includes
+  * [library reservation](https://github.com/maurofokker/spring-microservices-library-reservation)
+  * [library catalog](https://github.com/maurofokker/spring-microservices-library-catalog)
---
+++ src/main/java/com/maurofokker/poc/cloud/SpringMicroservicesLibraryUiApplication.java
@@ -0,0 +1,12 @@
+package com.maurofokker.poc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesLibraryUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservicesLibraryUiApplication.class, args);
+    }
+}
